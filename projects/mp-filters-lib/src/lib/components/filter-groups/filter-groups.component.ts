61c36dbece62cade1c536f20f8a6f3c0670ccff4
Michał Paduch (INI) <INI@kmd.dk>
2021-08-12 09:01:32 +0200

    Add expanded animation

--- projects/mp-filters-lib/src/lib/components/filter-groups/filter-groups.component.ts
+++ projects/mp-filters-lib/src/lib/components/filter-groups/filter-groups.component.ts
@@ -1,23 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, interval, Subject, Subscription } from 'rxjs';
 import { FilterGroupModel } from '../../models';
+import { map, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'mp-filter-groups',
   templateUrl: './filter-groups.component.html',
   styleUrls: ['./filter-groups.component.scss'],
 })
-export class FilterGroupsComponent implements OnInit {
+export class FilterGroupsComponent implements OnInit, OnDestroy {
 
   @Input()
   public filter!: FilterGroupModel;
   @Input()
-  public level: number = 0;
+  public level = 0;
 
   public expanded: boolean = false;
+
+  @ViewChild("content")
+  public content: ElementRef<any> | null = null;
+  public contentHeight$ = new BehaviorSubject<number>(0);
+  public interval$ = interval(100)
+    .pipe(
+      map(() => (this.content?.nativeElement as HTMLElement)?.offsetHeight ?? 0),
+    );
+  private subscriptions = new Subscription();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.subscriptions.add(this.interval$.subscribe(h => this.contentHeight$.next(h)));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   public toggleExpanded(): void {
